4692f6d5207f13c5561569ebc35fc064b5d72a44
Sergio Villa Carro <s__villa@MacBook-Air-de-Sergio.local>
2014-03-18 19:00:37 +0100

    Ejercicio 1

---
+++ Prueba.h
@@ -0,0 +1,13 @@
+//
+//  Prueba.h
+//  Ej1T1
+//
+//  Created by Sergio Villa Carro on 18/03/14.
+//  Copyright (c) 2014 Sergio Villa Carro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Prueba : NSObject
+
+@end
