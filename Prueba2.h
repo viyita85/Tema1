2259771745ac5e5d7cd32a79e9664e15b32d9aea
Sergio Villa Carro <s__villa@MacBook-Air-de-Sergio.local>
2014-03-18 19:09:57 +0100

    Segundo commit

---
+++ Prueba2.h
@@ -0,0 +1,13 @@
+//
+//  Prueba2.h
+//  Ej1T1
+//
+//  Created by Sergio Villa Carro on 18/03/14.
+//  Copyright (c) 2014 Sergio Villa Carro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Prueba2 : NSObject
+
+@end
---
+++ Prueba2.m
@@ -0,0 +1,13 @@
+//
+//  Prueba2.m
+//  Ej1T1
+//
+//  Created by Sergio Villa Carro on 18/03/14.
+//  Copyright (c) 2014 Sergio Villa Carro. All rights reserved.
+//
+
+#import "Prueba2.h"
+
+@implementation Prueba2
+
+@end
